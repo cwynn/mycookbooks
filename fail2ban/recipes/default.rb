4535fadf9fbd1844c226514f639e62ca55af234a
cwynn <cwynn@gascard.net>
2015-03-06 13:35:01 -0600

    Update default.rb

--- fail2ban/recipes/default.rb
+++ fail2ban/recipes/default.rb
@@ -1,1 +1,30 @@
 
+
+package "fail2ban" do
+  action :upgrade
+end
+
+service "fail2ban" do
+  supports [ :status => true, :restart => true ]
+  action [ :enable, :start ]
+end
+
+%w{ fail2ban jail }.each do |cfg|
+  template "/etc/fail2ban/#{cfg}.conf" do
+    source "#{cfg}.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, resources(:service => 'fail2ban')
+  end
+end
+
+%w{ nginx-auth nginx-proxy nginx-noscript }.each do |cfg|
+  template "/etc/fail2ban/filter.d/#{cfg}.conf" do
+    source "#{cfg}.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, resources(:service => 'fail2ban')
+  end
+end
